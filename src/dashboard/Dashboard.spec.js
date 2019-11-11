07d0c62ac068900942d86812c3f1e4e3b83923fa
Krunal Patel <Pkrunal616@gmail.com>
2019-11-11 16:02:06 -0500

    snapshot

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Dashboard from './Dashboard';
+
 // Test away
+
+test('This is working', () => {
+    expect(render(<Dashboard/>)).toMatchSnapshot();
+});
